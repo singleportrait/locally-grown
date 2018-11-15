2919637cd26dc99ea126d6f29857f6d0eed6e0d2
Jenn Scheer <singleportrait@gmail.com>
2018-11-14 16:53:58 -0800

    Show real channels & info on the channels index page!
    Woo!

--- src/Channels.js
+++ src/Channels.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
 
 import { padding, Header } from './styles';
 import WhatIsThisTooltip from './WhatIsThisTooltip';
@@ -29,13 +29,42 @@ class Channels extends Component {
     this.setState({ showInfo: !this.state.showInfo });
   }
 
-  render() {
+  renderChannel(channel, program, isMobile = false) {
+    const currentHour = this.props.session.currentHour;
+    const nextHour = currentHour + 1;
 
-    const multipliedChannels = this.props.featuredChannels.concat(this.props.featuredChannels.concat(this.props.featuredChannels.concat(this.props.featuredChannels)));
-    // TODO: Check if each featured program is in available programs.
-    // --Find program block for the current hour within (first?) active program--
-    // If not, we'll show something different:
-    // "Nothing playing right now for this channel."
+    const currentProgramBlock = program.fields.programBlocks.find(programBlock => programBlock.fields.startTime === currentHour);
+    // TODO: If there isn't anything playing, show the next thing that is
+    // const sortedProgramBlocks = getRelativeSortedProgramBlocks(this.props.programBlocks, this.props.currentHour);
+    // const nextProgramBlock = sortedProgramBlocks.shift();
+
+    return (
+      <Link to={channel.fields.slug} className={channelContainer}>
+        <ProgramContainer className={isMobile ? mobileProgramContainer : ''}>
+          <ProgramImage className={isMobile ? mobileProgramImage : ''} backgroundImage={program.fields.previewImage && program.fields.previewImage.fields.file.url} />
+          <ProgramInfo className={isMobile ? mobileProgramInfo : ''}>
+            { currentProgramBlock &&
+              <React.Fragment>
+                <h2>{currentProgramBlock.fields.title}</h2>
+                <h3>{moment(currentHour, "HH").format("h")}&ndash;{moment(nextHour, "HH").format("ha")}</h3>
+              </React.Fragment>
+            }
+            <h4>
+              {channel.fields.title}
+              {channel.fields.user &&
+                <div>by {channel.fields.user.fields.name}</div>
+              }
+            </h4>
+            { !currentProgramBlock &&
+              <div>Nothing playing at this hour.</div>
+            }
+          </ProgramInfo>
+        </ProgramContainer>
+      </Link>
+    );
+  }
+
+  render() {
     return (
       <ChannelsWrapper>
         <Header>
@@ -52,34 +81,16 @@ class Channels extends Component {
         <Link to="/tv-guide">TV Guide</Link>
         <MediaQuery minWidth={600}>
           <div>
-            { multipliedChannels.map(({fields}, i) =>
-              <Channel key={i}>
-                <ProgramImage src="" />
-                <ProgramInfo>
-                  <h2>Hourly Programming {i}</h2>
-                  <h3>5-6am</h3>
-                  <h4>{fields.title} by User</h4>
-                  <p>Note: Need to display whether something is playing for this hour</p>
-                </ProgramInfo>
-              </Channel>
-            )}
+            { this.props.featuredChannels.map((channel) => channel.fields.programs.map((program, i) =>
+              <React.Fragment key={i}>{this.renderChannel(channel, program)}</React.Fragment>
+            ))}
           </div>
         </MediaQuery>
         <MediaQuery maxWidth={600}>
           <div>
-            { multipliedChannels.map(({fields}, i) =>
-              <Channel key={i}>
-                <MobileProgramContainer>
-                  <MobileProgramImage src="" />
-                  <MobileProgramInfo>
-                    <h2>Hourly Programming {i}</h2>
-                    <h3>5-6am</h3>
-                    <h4>{fields.title} by User</h4>
-                    <p>Note: Need to display whether something is playing for this hour</p>
-                  </MobileProgramInfo>
-                </MobileProgramContainer>
-              </Channel>
-            )}
+            { this.props.featuredChannels.map((channel) => channel.fields.programs.map((program, i) =>
+              <React.Fragment key={i}>{this.renderChannel(channel, program, true)}</React.Fragment>
+            ))}
           </div>
         </MediaQuery>
       </ChannelsWrapper>
@@ -92,38 +103,60 @@ const ChannelsWrapper = styled('div')`
 `;
 
 // TODO: How can I target these rows better? Using modulo or a better formula?
-const Channel = styled('div')`
+// This only works up to 18 channels
+const channelContainer = css`
   display: flex;
   margin: ${padding} 0;
+  text-decoration: none;
+  position: relative;
   &:nth-child(2), &:nth-child(10) {
     padding-left: calc(15vw - 2.8rem);
   }
   &:nth-child(3), &:nth-child(11) {
     padding-left: calc(30vw - 2.8rem);
   }
-  &:nth-child(4), &:nth-child(5), &:nth-child(6), &:nth-child(7) {
-    flex-direction: row-reverse;
+  &:nth-child(4), &:nth-child(5), &:nth-child(6), &:nth-child(7),
+  &:nth-child(12), &:nth-child(13), &:nth-child(14), &:nth-child(15) {
+    &, > div { flex-direction: row-reverse; }
     text-align: right;
   }
-  &:nth-child(4) {
+  &:nth-child(4), &:nth-child(12) {
     padding-right: 15vw;
   }
-  &:nth-child(5) {
+  &:nth-child(5), &:nth-child(13) {
   }
-  &:nth-child(6) {
+  &:nth-child(6), &:nth-child(14) {
     padding-right: 15vw;
   }
-  &:nth-child(7) {
+  &:nth-child(7), &:nth-child(15) {
     padding-right: 30vw;
   }
-  &:nth-child(8) {
+  &:nth-child(8), &:nth-child(16) {
     padding-left: 15vw;
   }
 `;
 
-const ProgramImage = styled('img')`
+const ProgramContainer = styled('div')`
+  display: flex;
+`;
+
+const mobileProgramContainer = css`
+  position: relative;
+  display: block;
+  text-align: left;
+`;
+
+const ProgramImage = styled('div')`
   width: 40vw;
   height: 30vw;
+  background-image: url(${props => props.backgroundImage || './static_placeholder4.png'});
+  background-size: cover;
+`;
+
+const mobileProgramImage = css`
+  width: 60vw;
+  height: 45vw;
+  opacity: .3;
 `;
 
 const ProgramInfo = styled('div')`
@@ -131,19 +164,11 @@ const ProgramInfo = styled('div')`
   max-width: 30vw;
 `;
 
-const MobileProgramContainer = styled('div')`
-  position: relative;
-`;
-
-const MobileProgramImage = styled('img')`
-  width: 60vw;
-  height: 45vw;
-`;
-
-const MobileProgramInfo = styled('div')`
+const mobileProgramInfo = css`
   position: absolute;
   top: ${padding};
-  left: ${padding};
+  left: 0;
+  max-width: none;
 `;
 
 const mapStateToProps = state => ({
