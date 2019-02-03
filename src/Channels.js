a210d1dbf8e06e0ab7b7082998de0386dcd4fc24
Jenn Scheer <singleportrait@gmail.com>
2019-02-02 20:34:27 -0500

    Add styles support for channels with nothing playing on index page
    - Different messaging
    - Play what's up next/later
    - Show message on mobile

--- src/Channels.js
+++ src/Channels.js
@@ -9,6 +9,8 @@ import { padding, Header } from './styles';
 import WhatIsThisTooltip from './WhatIsThisTooltip';
 import TVGuideLink from './TVGuideLink';
 
+import { getRelativeSortedProgramBlocks } from './programBlockHelpers';
+
 import * as moment from 'moment';
 
 class Channels extends Component {
@@ -33,21 +35,43 @@ class Channels extends Component {
   renderChannel(channel, program, isMobile = false) {
     const currentHour = this.props.session.currentHour;
     const nextHour = currentHour + 1;
+    let nextProgramBlock = null;
 
     const currentProgramBlock = program.fields.programBlocks.find(programBlock => programBlock.fields.startTime === currentHour);
     // TODO: If there isn't anything playing, show the next thing that is
     // const sortedProgramBlocks = getRelativeSortedProgramBlocks(this.props.programBlocks, this.props.currentHour);
     // const nextProgramBlock = sortedProgramBlocks.shift();
+    const programClasses = isMobile ? mobileProgramImageContainerStyle : undefined;
+    const imageClasses = isMobile || !currentProgramBlock ? textOverImageStyle : undefined;
+
+    if (!currentProgramBlock) {
+      const sortedProgramBlocks = getRelativeSortedProgramBlocks(program.fields.programBlocks, currentHour);
+
+      nextProgramBlock = sortedProgramBlocks.shift();
+    }
 
     return (
       <Link to={channel.fields.slug} className={channelContainer}>
         <ProgramContainer className={isMobile ? mobileProgramContainer : ''}>
-          <ProgramImage className={isMobile ? mobileProgramImage : ''} backgroundImage={program.fields.previewImage && program.fields.previewImage.fields.file.url} />
+          <ProgramImageContainer className={programClasses}>
+            <ProgramImage className={imageClasses} backgroundImage={program.fields.previewImage && program.fields.previewImage.fields.file.url} />
+            { !currentProgramBlock && !isMobile &&
+              <NoCurrentProgramBlockText>
+                Nothing playing on this channel right now!
+              </NoCurrentProgramBlockText>
+            }
+          </ProgramImageContainer>
           <ProgramInfo className={isMobile ? mobileProgramInfo : ''}>
             { currentProgramBlock &&
               <React.Fragment>
                 <h2>{currentProgramBlock.fields.title}</h2>
-                <h3>{moment(currentHour, "HH").format("h")}&ndash;{moment(nextHour, "HH").format("ha")}</h3>
+                <h3 className={isMobile ? undefined : timeWindowStyle}>{moment(currentHour, "HH").format("h")}&ndash;{moment(nextHour, "HH").format("ha")}</h3>
+              </React.Fragment>
+            }
+            { !currentProgramBlock && nextProgramBlock &&
+              <React.Fragment>
+                <h2>Next up: {nextProgramBlock.fields.title}</h2>
+                <h3 className={isMobile ? undefined : timeWindowStyle}>{moment(nextProgramBlock.fields.startTime, "HH").format("h")}&ndash;{moment(nextProgramBlock.fields.startTime + 1, "HH").format("ha")}</h3>
               </React.Fragment>
             }
             <h4>
@@ -56,8 +80,8 @@ class Channels extends Component {
                 <div>by {channel.fields.user.fields.name}</div>
               }
             </h4>
-            { !currentProgramBlock &&
-              <div>Nothing playing at this hour.</div>
+            { !currentProgramBlock && isMobile &&
+              <p>Nothing playing on this channel right now!</p>
             }
           </ProgramInfo>
         </ProgramContainer>
@@ -152,17 +176,42 @@ const mobileProgramContainer = css`
   text-align: left;
 `;
 
-const ProgramImage = styled('div')`
+const ProgramImageContainer = styled('div')`
   width: 40vw;
   height: 30vw;
+  position: relative;
+`;
+
+const mobileProgramImageContainerStyle = css`
+  width: 60vw;
+  height: 45vw;
+`;
+
+const ProgramImage = styled('div')`
+  width: 100%;
+  height: 100%;
   background-image: url(${props => props.backgroundImage || './static_placeholder4.png'});
+  // background-image: url('samurai.png');
   background-size: cover;
 `;
 
-const mobileProgramImage = css`
-  width: 60vw;
-  height: 45vw;
+const textOverImageStyle = css`
   opacity: .3;
+`;
+
+const NoCurrentProgramBlockText = styled('h4')`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const timeWindowStyle = css`
+  margin: 1rem 0;
 `;
 
 const ProgramInfo = styled('div')`
@@ -172,7 +221,8 @@ const ProgramInfo = styled('div')`
 
 const mobileProgramInfo = css`
   position: absolute;
-  top: ${padding};
+  padding: 0 1rem;
+  top: .75rem;
   left: 0;
   max-width: none;
 `;
