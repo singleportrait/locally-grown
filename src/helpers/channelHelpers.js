73fe2c5f9b36c99d79ceffd1bb3190f2c3584bd5
Jenn Scheer <singleportrait@gmail.com>
2020-08-17 18:56:52 -0400

    Clean up repeatProgramBlocks helper

--- src/helpers/channelHelpers.js
+++ src/helpers/channelHelpers.js
@@ -1,24 +1,23 @@
 import clone from 'rfdc';
 
 export const repeatProgramBlocks = (programBlocks) => {
-  const programBlocksTotal = programBlocks.length - 1;
-
-  let repeatedProgramBlocks = [];
+  const total = programBlocks.length - 1;
 
   // Loop through existing program blocks to add to array
   // Set their hours to be 0, 1, 2, ... until there are no more
   // Loop through the programs, increasing hours until we get to 23
+  let repeatedProgramBlocks = [];
   let i = 0;
   let hour = 0;
+
   while (hour < 24) {
-    // consoleLog("i: ", i, "hour: ", hour);
     // Note: Deep-copying object
-    const repeatedProgramBlock = clone()(programBlocks[i]);
-    repeatedProgramBlock.fields.startTime = hour;
-    repeatedProgramBlocks.push(repeatedProgramBlock);
+    const _block = clone()(programBlocks[i]);
+    _block.fields.startTime = hour;
 
-    i < programBlocksTotal ? i++ : i = 0;
+    repeatedProgramBlocks.push(_block);
 
+    i < total ? i++ : i = 0;
     hour++;
   }
   return repeatedProgramBlocks;
