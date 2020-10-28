62f541d526c2c8443088c7d7103410ce7020671b
Jenn Scheer <singleportrait@gmail.com>
2020-10-28 17:23:45 -0400

    Initial user object on screenings page

--- src/Screening.js
+++ src/Screening.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { UserContext } from "./providers/UserProvider";
+
 function Screening(props) {
+  const { user } = useContext(UserContext);
+
   return (
     <div style={{padding: "1rem"}}>
       <Link to="/screenings">Back to screenings</Link>
@@ -9,6 +13,19 @@ function Screening(props) {
       <br />
       Individual screening page:
       <h1>{ props.screening.fields.title }</h1>
+      { !user &&
+        <>
+          <h4>Not signed in :)</h4>
+          {/* <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}/> */}
+        </>
+      }
+      { user &&
+        <>
+          <h4>Loaded! Hello { user.displayName }</h4>
+          {/* <p style={linkStyle} onClick={() => auth.signOut()}>Sign out</p> */}
+          <br />
+        </>
+      }
     </div>
   );
 }
