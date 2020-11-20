963e82873ff1edb1e92f3db82a265cc07aef1cf1
Jenn Scheer <singleportrait@gmail.com>
2020-11-19 20:00:01 -0500

    Try adding service worker back

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
+registerServiceWorker();
